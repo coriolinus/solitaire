77855c9c916dda99539288550184af17b1afbff1
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-02-28 23:49:51 +0100

    fix tests which were accidentally broken in iface chg

--- src/deck.rs
+++ src/deck.rs
@@ -289,15 +289,15 @@ mod small_deck_tests {
     fn test_count_cut() {
         let mut deck: Deck = (1..=(DECK_SIZE as u8)).rev().collect();
         assert_eq!(deck.0, [8, 7, 6, 5, 4, 3, 2, 1]);
-        deck.count_cut();
+        deck.count_cut(None);
         assert_eq!(deck.0, [7, 6, 5, 4, 3, 2, 8, 1]);
         deck.push(2, 2);
         assert_eq!(deck.0, [7, 6, 5, 4, 3, 8, 1, 2]);
-        deck.count_cut();
+        deck.count_cut(None);
         assert_eq!(deck.0, [5, 4, 3, 8, 1, 7, 6, 2]);
         deck.push(3, 5);
         assert_eq!(deck.0, [5, 4, 8, 1, 7, 6, 2, 3]);
-        deck.count_cut();
+        deck.count_cut(None);
         assert_eq!(deck.0, [1, 7, 6, 2, 5, 4, 8, 3]);
     }
 
@@ -305,7 +305,7 @@ mod small_deck_tests {
     fn test_count_cut_joker_semantics() {
         let mut deck = Deck::new();
         assert_eq!(deck.0, [1, 2, 3, 4, 5, 6, 7, 8]);
-        deck.count_cut();
+        deck.count_cut(None);
         assert_eq!(deck.0, [1, 2, 3, 4, 5, 6, 7, 8]);
     }
 }
